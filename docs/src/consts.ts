ddddbbdb26a13bf9fe26fb1df9bc23892b4e64d6
Dravia Vemal <contact@draviavemal.com>
2025-10-03 16:05:16 +0000

    Refactor components and styles for improved consistency and clarity; update site title and description, remove unused about page, and enhance global styles.

--- docs/src/consts.ts
+++ docs/src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Dravia Vemal Docs';
+export const SITE_DESCRIPTION = 'Document collection of Dravia Vemal\'s projects and resources.';
